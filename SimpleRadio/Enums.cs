c2284a059e1d3f4509b9d001e07e73c6bc365eeb
Hannele Ruiz <justlemoncl@gmail.com>
2018-12-16 03:05:43 -0300

    Added enum for the radio type

---
+++ SimpleRadio/Enums.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleRadio
+{
+    /// <summary>
+    /// Type of rhe added radio.
+    /// </summary>
+    public enum RadioType
+    {
+        /// <summary>
+        /// An internal vanilla game radio.
+        /// </summary>
+        Vanilla = 0,
+        /// <summary>
+        /// A local file stored on your PC.
+        /// </summary>
+        Local = 1,
+        /// <summary>
+        /// An MP3 radio stream.
+        /// </summary>
+        Stream = 2
+    }
+}
